55bc199c26d0c788e9cdb51dcff2781df053829a
Pavel dub <dubstr1@gmail.com>
2019-11-03 22:15:37 +0300

    shop item component and 1 page created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Switch ,Route } from 'react-router-dom';
 import './App.css';
+
+//main pages
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
-      <HomePage />
+      <div>
+        <Switch>
+          <Route exact path='/' component={ HomePage }/>
+          <Route path='/shop' component={ ShopPage }/>
+        </Switch>
+      </div>
   );
 }
 
