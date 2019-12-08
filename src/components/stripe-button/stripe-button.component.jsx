4a0ada5cf270094e8f67ad1da75d81b0e9330959
Pavel dub <dubstr1@gmail.com>
2019-12-08 19:22:22 +0300

    stripe integrated

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({price}) => {
+    const priceForStripe = price * 100;
+    const publishebleKey = 'pk_test_gY2EtU5XFpGzNKUwgFMoBkzK00OcUqqkZW';
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful')
+    }
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            namee='CRWN Clothing'
+            billingAddress
+            shippingAddress
+            image='https://sendeyo.com/up/d/f3eb2117da'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishebleKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton;
