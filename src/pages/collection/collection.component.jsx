e73c07026a056a46cd6eb9ba51fb57692be4285c
Pavel dub <dubstr1@gmail.com>
2019-12-08 16:47:48 +0300

    separate pages

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -2,17 +2,22 @@ import React from 'react';
 import { connect } from 'react-redux';
 import './collection.styles.scss';
 
-import CollectionItem from '../../components/collection-item/collection-item.component';
 import { selectCollection } from '../../redux/shop/shop.selectors';
+import CollectionItem from '../../components/cart-item/cart-item.component';
 
-const CollectionPage = ({ match }) => (
-	<div className="collection-page">
-		<h2>Collection Page</h2>
-	</div>
-);
+const CollectionPage = ({ collection }) => {
+	const { title, items } = collection;
+
+	return (
+		<div className="collection-page">
+			<h2 className="title">{title}</h2>
+			<div className="items">{items.map((item) => <CollectionItem key={item.id} item={item} />)}</div>
+		</div>
+	);
+};
 
 const mapStateToProps = (state, ownProps) => ({
-	collections: selectCollection(ownProps.match.params.collectionId)(state)
+	collection: selectCollection(ownProps.match.params.collectionId)(state)
 });
 
 export default connect(mapStateToProps)(CollectionPage);
