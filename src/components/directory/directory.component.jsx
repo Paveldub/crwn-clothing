6949781858bef8a4b1844ee9be31f1579e697d41
Pavel dub <dubstr1@gmail.com>
2019-12-08 18:27:25 +0300

    our store divided in separate independent parts

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -2,20 +2,22 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
+
 import MenuItem from '../menu-item/menu-item.component';
-import { selectDirectorySections } from '../../redux/directory/directory.selectosr';
 
 import './directory.styles.scss';
 
 const Directory = ({ sections }) => (
-	<div className="directory-menu">
-		{sections.map(({ id, ...otherSectionProps }) => <MenuItem key={id} {...otherSectionProps} />)}
-	</div>
+  <div className='directory-menu'>
+    {sections.map(({ id, ...otherSectionProps }) => (
+      <MenuItem key={id} {...otherSectionProps} />
+    ))}
+  </div>
 );
 
-const mapStateToProps = () =>
-	createStructuredSelector({
-		sections: selectDirectorySections
-	});
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections
+});
 
 export default connect(mapStateToProps)(Directory);
