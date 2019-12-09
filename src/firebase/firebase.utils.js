e19baeeeaf553e3f6a7a6e9e9fa86904ac318dfd
Pavel dub <dubstr1@gmail.com>
2019-12-09 22:18:32 +0300

    firebase config adjustments

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+    apiKey: "AIzaSyCBWhbwzQqOz3hiYxSTEwQJrmjQ5EwMKKA",
+    authDomain: "crwn-shop-database.firebaseapp.com",
+    databaseURL: "https://crwn-shop-database.firebaseio.com",
+    projectId: "crwn-shop-database",
+    storageBucket: "crwn-shop-database.appspot.com",
+    messagingSenderId: "783522600657",
+    appId: "1:783522600657:web:493e55002d4b6261f617f0"
 };
 
 firebase.initializeApp(config);
